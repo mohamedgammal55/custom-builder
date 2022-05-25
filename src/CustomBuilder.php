b8d2f02234bd92b64b6326e52142da86279594b2
mohamed gamal <devmohamedgamal919@gmail.com>
2022-05-25 16:51:33 +0200

    first commit

---
+++ src/CustomBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Builders;
+use Illuminate\Database\Eloquent\Builder;
+
+class CustomBuilder extends Builder
+{
+    public function createOrDelete(array $attributes, array $values = [])
+    {
+        return tap($this->deleteOrNew($attributes), function ($instance) use ($values) {
+            $instance->fill($values)->save();
+        });
+    }//end fun
+
+    public function deleteOrNew(array $attributes = [], array $values = [])
+    {
+        if (! is_null($instance = $this->where($attributes)->first())) {
+            $this->where($attributes)->delete();
+            return $instance;
+        }
+
+        return $this->newModelInstance($attributes + $values);
+    }
+}//end fun
